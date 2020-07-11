50e6099d3ed521e69261a38bc597bd71703ad853
Green-Avocado <greenavocado@protonmail.com>
2020-07-11 15:26:33 -0600

    feat: added requests library

--- server/server.py
+++ server/server.py
@@ -3,12 +3,22 @@
 import flask
 from flask import request, jsonify
 
+import requests
+
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+
+apiKey = "AIzaSyCiO1LK78bn5NYUkqytbXrUb-d-dbqPK5o"
+baseURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=" + apiKey
 
 @app.route('/', methods=['GET'])
 def home():
     return "Hello, world!"
+
+@app.route('/api/places', method=['GET'])
+def places():
+    res = []
+    return jsonify(res)
 
 app.run()
 
