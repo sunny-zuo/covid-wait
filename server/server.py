ea2a40358ed0bfec91f617f5f5a813f68ed9bda2
Green-Avocado <greenavocado@protonmail.com>
2020-07-11 14:50:22 -0600

    feat: added template code for flask

--- server/server.py
+++ server/server.py
@@ -1,2 +1,14 @@
-#/usr/bin/python
+#!/usr/bin/python
+
+import flask
+from flask import request, jsonify
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/', methods=['GET'])
+def home():
+    return "Hello, world!"
+
+app.run()
 
