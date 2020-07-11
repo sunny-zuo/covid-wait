5a792e2bf06643db3c7273bb76a6a6c85c7001eb
sunny-zuo <me@sunnyzuo.com>
2020-07-11 16:34:55 -0600

    feat: add base HTML

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,12 +1,58 @@
 import React from "react"
 import Layout from "../components/Layout"
 
-export default function Home() {
-  return (
-    <div>
-      <Layout>
-        <p>Hello world!</p>
-      </Layout>
-    </div>
-  )
+class Home extends React.Component {
+  componentDidMount() {
+    /*
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        console.log(`${position.coords.latitude}, ${position.coords.longitude}`)
+      });
+    }*/
+  }
+
+  render() {
+    return (
+      <div>
+        <Layout>
+          <h3>Find the least crowded, and safest grocery stores (or other place types) in your area. Updated in real time.</h3>
+          <form>
+            <input type="text" name="address" aria-label="User Address" placeholder="Enter Your Address"></input>
+            <br/>
+            <label>Find:</label>
+            <select id="placeType" name="placeType">
+              <option value="supermarket">Supermarket</option>
+              <option value="drugstore">Drug Store</option>
+              <option value="department_store">Department Store</option>
+            </select> 
+          </form>
+          <button onClick={makeRequestAddy}>Go!</button>
+
+          <br/>
+          <button onClick={getLocation}>Click here to use your current location</button>
+        </Layout>
+      </div>
+    )
+  }
 }
+
+function getLocation() {
+  if (navigator.geolocation) {
+    console.log('has geolocation');
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(`coords: ${position.coords.latitude}, ${position.coords.longitude}`)
+    }, (error) => {console.log(error)});
+  } else {
+    console.log('no geolocation')
+  }
+}
+
+function makeRequestCoords(position) {
+
+}
+
+function makeRequestAddy() {
+
+}
+
+export default Home;
