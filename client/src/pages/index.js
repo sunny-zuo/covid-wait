d47e3683ecd9dc71ef306f0036a81349482901a3
Cheryl <44687824+churrowousers.noreply.github.com>
2020-07-11 16:28:54 -0600

    Input Bar
    allows user to input location

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,11 +1,26 @@
 import React from "react"
 import Layout from "../components/Layout"
+import "../stylesheets/index.css"
 
 export default function Home() {
   return (
     <div>
       <Layout>
-        <p>Hello world!</p>
+        <div class="nav">
+            <p>Home</p>
+            <p>About</p>
+            <p>Check Stores</p>
+            <p>Covid Safety Tips</p>
+        </div>
+        <div class="about">
+          <h2>About COVID Wait</h2>
+        </div>
+        <div class="Check Stores">
+          <h2>Check Nearby Stores</h2>
+          <form>
+            <input type="text" placeholder="Enter Location"></input>
+          </form>
+        </div>
       </Layout>
     </div>
   )
