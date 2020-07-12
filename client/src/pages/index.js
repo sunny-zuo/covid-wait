ee5549db6c39043f49d5d132f7225123b3fd441c
Cheryl <44687824+churrowousers.noreply.github.com>
2020-07-11 18:38:15 -0600

    Style update
    created boxes for safety tips

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -15,11 +15,12 @@ class Home extends React.Component {
   render() {
     return (
       <div>
+        <a id="top"></a>
         <Layout>
         <div className="nav">
-            <p>Home</p>
-            <p>Check Stores</p>
-            <p>Covid Safety Tips</p>
+            <a href="#top">Home</a>
+            <a href="#search">Check Stores</a>
+            <a href="#tips">Covid Safety Tips</a>
         </div>
           <div className="Check Stores">
           <h3>Find the least crowded, and safest grocery stores (or other place types) in your area. Updated in real time.</h3>
@@ -51,16 +52,16 @@ class Home extends React.Component {
         <div className="tips">
           <h2>Tips to Stay Safe While Grocery Shopping</h2>
           <h3>To help protect yourself, essential grocery store workers, and other shoppers, keep these tips in mind:</h3>
-          <ol>
-            <li>Avoid going to stores if you're sick, even with mild symptoms</li>
-            <li>Wear a face mask. Some stores may make face coverings mandatory.</li>
-            <li>Go alone and avoid going during peak times</li>
-            <li>Practice social distancing by keeping at least 2 metres (6 ft) apart from other shoppers and employees.</li>
-            <li>Resist the urge to panic buy, and instead only purchase 1-2 weeks worth of groceries at a time in order to avoid creating unnecessary demand and temporary shortages.</li>
-            <li>Prepare a shopping list to help minimize the amount of time you spend in the store</li>
-            <li>Wash your hands with warm water and soap before heading out, when you return home, and after putting away your groceries. If soap and water are not avaliable, use alcohol based hand sanitizer that contains at leasy 60% alcohol. Make sure to keep your hands away from your face.</li>
-            <li>Only handle produce you intend to buy</li>
-            <li>Use disinfectant wipes (either use the ones provided by the store or bring your own) to wipe down the handles of your shopping cart or basket. If you choose to use reusable shopping bags, make sure that they area cleaned and washed before/after every use.</li>
+          <ol id="tips">
+            <li className="box1">Avoid going to stores if you're sick, even with mild symptoms</li>
+            <li className="box2">Wear a face mask. Some stores may make face coverings mandatory.</li>
+            <li className="box1">Go alone and avoid going during peak times</li>
+            <li className="box2">Practice social distancing by keeping at least 2 metres (6 ft) apart from other shoppers and employees.</li>
+            <li className="box1">Resist the urge to panic buy, and instead only purchase 1-2 weeks worth of groceries at a time in order to avoid creating unnecessary demand and temporary shortages.</li>
+            <li className="box2">Prepare a shopping list to help minimize the amount of time you spend in the store</li>
+            <li className="box1">Wash your hands with warm water and soap before heading out, when you return home, and after putting away your groceries. If soap and water are not avaliable, use alcohol based hand sanitizer that contains at leasy 60% alcohol. Make sure to keep your hands away from your face.</li>
+            <li className="box2">Only handle produce you intend to buy</li>
+            <li className="box1">Use disinfectant wipes (either use the ones provided by the store or bring your own) to wipe down the handles of your shopping cart or basket. If you choose to use reusable shopping bags, make sure that they area cleaned and washed before/after every use.</li>
           </ol>
         </div>
         </Layout>
