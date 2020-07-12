9c54e34dab677aa1a45f256a7ca477e1e16b6011
sunny-zuo <me@sunnyzuo.com>
2020-07-12 00:18:10 -0600

    feat: get crowdedness rating

--- client/src/components/storeCard.js
+++ client/src/components/storeCard.js
@@ -4,6 +4,21 @@ import "./storeCard.css"
 class StoreCard extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            busy: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch(`http://127.0.0.1:5000/api/popularity?place_id=${this.props.placeID}`, {
+            method: 'GET'
+        }).then(response => response.json())
+        .then(data => {
+            console.log(data);
+            this.setState({busy: data.result?.current_popularity});
+        }).catch(error => {
+            this.setState({busy: 'Error getting data'});
+        })
     }
 
     render() {
@@ -12,6 +27,7 @@ class StoreCard extends React.Component {
                 <span className={this.props.opening_hours.open_now ? "openStatus open" : "openStatus closed"}>{this.props.opening_hours.open_now ? "Open" : "Closed"}</span>
                 <h1>{this.props.storeName}</h1>
                 <h2>{this.props.vicinity}</h2>
+                <h3>Crowdedness Rating: {this.state.busy}</h3>
             </div>
         )
     }
